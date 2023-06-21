3ede80fe21f131ad93164bc455682facb610dcd2
Halo <44603549+HaloKim@users.noreply.github.com>
2023-06-21 09:21:27 +0900

    Update falcon_tune.sh

--- falcon_tune.sh
+++ falcon_tune.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
+pip install --no-deps -r requirements.txt
 python setup.py install
 
 # urllib3 에러 발생시
@@ -7,7 +7,7 @@ pip install 'urllib3<2'
 falcontune finetune \
     --model=falcon-7b-instruct \
     --weights=tiiuae/falcon-7b-instruct \
-    --dataset=./KoAlpaca_v1.1.json \
+    --dataset=/home/jovyan/KoFalcontune/KoAlpaca_v1.1.jsonl \
     --data_type=alpaca \
     --lora_out_dir=./falcon-7b-instruct-alpaca/ \
     --mbatch_size=1 \
